2a4a31c2a80d285511b4c3155a50bd3f9d647cf5
Eric Nograles <ENograles@352inc.com>
2016-10-21 07:08:32 -0400

    implements amqp service

---
+++ index.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+
+/**
+ * A sample AMQP microservice for service bus communication via RabbitMQ with the seneca-service-api
+ * @param options
+ */
+function twitter(options) {
+  var log;
+
+  this.add('init:twitter', init);
+  this.add('role:twitter,cmd:*', noMatch);
+  this.add('role:twitter,cmd:tweets', tweets);
+
+  function init(msg, respond) {
+    // Note, all the code below is optional
+    // log to a custom file
+    fs.open(options.logfile, 'a', function (err, fd) {
+
+      // cannot open for writing, so fail
+      // this error is fatal to Seneca
+      if (err) return respond(err);
+
+      log = makeLog(fd);
+      respond();
+    });
+  }
+
+  function makeLog(fd) {
+    // TODO: Tie this into something like Winston
+    return function (entry) {
+      fs.write(fd, '\n' + new Date().toISOString() + ' ' + entry, null, 'utf8', function (err) {
+        if (err) return console.log(err);
+
+        // ensure log entry is flushed
+        fs.fsync(fd, function (err) {
+          if (err) return console.log(err);
+        })
+      });
+    }
+  }
+
+  function noMatch(payload, respond) {
+    respond(null, {
+      statusCode: 404,
+      original: payload,
+      error: {
+        message: 'Invalid service command'
+      }
+    });
+  }
+
+  function tweets(payload, respond) {
+    // TODO: Your service begins here
+    log('RECEIVED: ' + payload);
+    respond(null, {status: 'success', tweets: []});
+  }
+}
+
+// Define queues
+require('seneca')()
+  .use(twitter, { logfile: './twitter.log'})
+  .use('seneca-amqp-transport')
+  .listen({
+    type: 'amqp',
+    pin: 'role:twitter,cmd:*',
+    url: process.env.AMQP_URL || 'amqp://127.0.0.1'
+  });
